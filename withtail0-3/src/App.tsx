b96e3e011dc0247d257e4c03ea5097e290df65e4
seokangho5062 <seokangho5062@gmail.com>
2025-12-25 18:17:18 +0900

    병원/장례 초기화면 beta

--- withtail0-3/src/App.tsx
+++ withtail0-3/src/App.tsx
@@ -5,9 +5,6 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage0 />} />
-      {/* 추후 연결 */}
-      {/* <Route path="/hospital" element={<HospitalPage />} /> */}
-      {/* <Route path="/funeral" element={<FuneralPage />} /> */}
     </Routes>
   );
 }
--- withtail0-3/src/pages/MainPage0.tsx
+++ withtail0-3/src/pages/MainPage0.tsx
@@ -6,7 +6,6 @@ import "./MainPage0.css";
 export default function MainPage0() {
   return (
     <div className="mp0-root">
-      {/* 1280 x 832 고정 + 내부 스크롤 */}
       <div className="mp0-viewport">
         <Header />
 
@@ -20,10 +19,13 @@ export default function MainPage0() {
 
           <section className="mp0-cards">
             <article className="mp0-card">
-              <div className="mp0-iconCircle mp0-iconCircle--blue"><img src = "../../images/Vector.png"></img></div>
+              <div className="mp0-iconCircle mp0-iconCircle--blue">
+                <img src="../../images/Vector.png" />
+              </div>
               <h2 className="mp0-cardTitle">동물병원 찾기</h2>
               <p className="mp0-cardDesc">
-                반려동물의 건강을 위한 최고의 동물병원을<br />
+                반려동물의 건강을 위한 최고의 동물병원을
+                <br />
                 찾아보세요
               </p>
               <Link to="/hospital" className="mp0-btn mp0-btn--blue">
@@ -32,10 +34,13 @@ export default function MainPage0() {
             </article>
 
             <article className="mp0-card">
-              <div className="mp0-iconCircle mp0-iconCircle--purple"><img src = "../../images/mdi_flower-outline.png"></img></div>
+              <div className="mp0-iconCircle mp0-iconCircle--purple">
+                <img src="../../images/mdi_flower-outline.png" />
+              </div>
               <h2 className="mp0-cardTitle">장례식장 찾기</h2>
               <p className="mp0-cardDesc">
-                소중한 반려동물을 위한 따뜻한 장례식장을<br />
+                소중한 반려동물을 위한 따뜻한 장례식장을
+                <br />
                 찾아보세요
               </p>
               <Link to="/funeral" className="mp0-btn mp0-btn--purple">
@@ -44,14 +49,18 @@ export default function MainPage0() {
             </article>
           </section>
 
-          {/* WithTail 소개 영역 */}
           <section className="mp0-about">
             <h3 className="mp0-aboutTitle">WithTail에 대해</h3>
             <p className="mp0-aboutText">
-              WithTail은 반려동물과 보호자의 삶 전반을 함께하는 플랫폼입니다.<br></br>
-              질병과 치료가 필요한 순간에는 믿을 수 있는 동물병원을,<br></br>
-              이별의 순간에는 정성 어린 장례식장을 안내합니다.<br></br>
-              정보의 혼란 속에서 고민하지 않도록 WithTail이 가장 필요한 선택을 돕습니다. <br></br>
+              WithTail은 반려동물과 보호자의 삶 전반을 함께하는 플랫폼입니다.
+              <br />
+              질병과 치료가 필요한 순간에는 믿을 수 있는 동물병원을,
+              <br />
+              이별의 순간에는 정성 어린 장례식장을 안내합니다.
+              <br />
+              정보의 혼란 속에서 고민하지 않도록 WithTail이 가장 필요한 선택을
+              돕습니다.
+              <br />
             </p>
           </section>
         </main>
