52fb48c0ad6c94c03290719614b18124e96aa007
Seo Kangho <seokangho5062@gmail.com>
2025-12-25 10:53:00 +0900

    초기 화면 beta

---
+++ withtail00/src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="brand">
+        {/* ✅ 로고 자리: 네 로고 생기면 여기에 img 넣으면 됨 */}
+        <div className="logoSlot" />
+        <span className="brandText">WithTail</span>
+      </div>
+    </header>
+  );
+}
