cda673b32fb0201dc27fa76282ec7693056874a6
Mean55 <wkvhr123@naver.com>
2025-12-29 00:39:50 +0900

    feat: signup routing flow

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,6 +4,9 @@ import App from "../App";
 // pages
 import Home from "../pages/Home/Home";
 import LoginPage from "../pages/login/LoginPage";
+import SignupTerms from "../pages/login/SignupTerms";
+import SignupInfo from "../pages/login/SignupInfo";
+import SignupVerify from "../pages/login/SignupVerify";
 import Mainpage01 from "../pages/mainpage01/mainpage01";
 import Mainpage02 from "../pages/mainpage02/mainpage02";
 import MyPageProfile from "../pages/mypage/MyPageProfile";
@@ -17,6 +20,15 @@ const router = createBrowserRouter([
       { index: true, element: <Home /> },
 
       { path: "login", element: <LoginPage /> },
+         {
+        path: "signup",
+        children: [
+          { path: "terms", element: <SignupTerms /> },   // /signup/terms
+          { path: "info", element: <SignupInfo /> },     // /signup/info
+          { path: "verify", element: <SignupVerify /> }, // /signup/verify
+        ],
+    },
+      
 
       {
         path: "hospital",
